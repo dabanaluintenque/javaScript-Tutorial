2958a23e1ba6afbbfdfbbf51d34902ff6d918f6b
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2022-03-08 17:01:25 -0500

    tutorial

---
+++ addition/app.js
@@ -0,0 +1,8 @@
+
+     function addition(x, y) {
+
+            const result = x + y;
+            return result;
+        }
+
+        document.getElementById("printResult").innerHTML ="The value of 7 + 7 = "+ addition(7, 7);

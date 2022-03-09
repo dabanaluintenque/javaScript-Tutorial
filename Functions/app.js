c76969a66bbc527255e30cf952c0f7910bee59a7
Dabana Intenque <64395601+dabanaluintenque@users.noreply.github.com>
2022-03-09 12:38:57 -0500

    Dabana's tutorial

---
+++ Functions/app.js
@@ -0,0 +1,29 @@
+
+function regularFunction(){
+
+let message ="Wy Functions: you can reuse code Define the code once, and use it many times."
+
+document.getElementById('example1').innerHTML= message;
+}
+
+function returnFunction(firstName, lastName){
+
+
+  let fullName = document.getElementById('example2').innerHTML=firstName+" "+lastName;
+
+    return fullName;
+}
+
+function convertToGigabytes(mega){
+
+ document.getElementById('example3').innerHTML= " To convert from megabytes to gigabytes, Multiply your"+
+ " figure by 0.001 (or divide by 1000) In this example we will use division. To converty 1000000MGB"
+
+
+let result = document.getElementById("answer").innerHTML= (mega/1000)+"GB";
+
+return result;
+ 
+}
+
+
